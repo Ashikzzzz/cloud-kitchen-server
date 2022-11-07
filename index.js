18839b88daa63704873c3d25e1b4d2a432006fd8
Ashikzzzz <ashik.emon5.im@gmail.com>
2022-11-08 01:25:47 +0600

    all service api ready

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ app.use(express.json());
 // database
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0zgm21v.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("cloudKitchenUser")
+      .collection("services");
+    //   services api
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("cloud kitchen");
